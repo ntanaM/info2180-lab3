c2f6267ddb36e6da15ac3aac7f402362480a67be
Dana Archer <danamarcher@gmail.com>
2024-10-27 13:43:57 -0500

    Added changes for Exercise 5

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -17,6 +17,18 @@ document.addEventListener(`DOMContentLoaded`, function(){
 
     // Initialize variables
 
+    // Winning combinations
+    const toWin = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
     // Set player X and O as constants
     const player_X = "X";
     const player_O = "O";
@@ -26,6 +38,27 @@ document.addEventListener(`DOMContentLoaded`, function(){
 
     // Set Array to keep track of board
     let board = Array(9).fill(null);
+
+    // Initalize functions
+    function checkWinner(player){ // Checks for a winner
+        for(const combination of toWin){
+            let [a, b, c] = combination;
+            if(board[a] === current_player && board[b] === current_player && board[c] === current_player){
+                return true;
+            }
+        }
+
+        return false
+    }
+
+
+    function checkDraw(player){ // Checks for a draw
+        if (checkWinner(player) == false && board.every(square => square !== null)){
+            return true;
+        }
+
+        return false;
+    }
 
 
     // Set functionality for squares on click
@@ -53,7 +86,22 @@ document.addEventListener(`DOMContentLoaded`, function(){
                 currentSquare.classList.add(current_player) // Adds proper styling 
                 board[index] = current_player
     
-                current_player =  current_player === player_X? player_O:player_X; // Alternates between X and O
+                // Exercise 4: Check for a winner and update the status
+                if (checkWinner(current_player) == true){
+                    gameStatus.textContent = `Congratulations ${current_player}! You are the winner :D`;
+                    gameStatus.classList.add(`you-won`);
+                    return 
+                }
+
+                else if (checkDraw(current_player) == true){ // Checks for a draw
+                    gameStatus.textContent = "It's a draw!";
+                    gameStatus.classList.add(`you-won`);
+                    return
+                }
+
+                else{
+                    current_player =  current_player === player_X? player_O:player_X; // Alternates between X and O
+                }
             }
 
         });
