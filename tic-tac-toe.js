b26d85a43d93133adb1a389d75e0ed4e3c84e0f8
Dana Archer <danamarcher@gmail.com>
2024-10-27 13:17:08 -0500

    Edited js file for exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,6 +14,39 @@ document.addEventListener(`DOMContentLoaded`, function(){
     boardSquares.forEach(square => square.classList.add(`square`));
 
 
+    // Exercise 2: Add an X or O square to the board
+
+    // Initialize variables
+
+    // Set player X and O as constants
+    const player_X = "X";
+    const player_O = "O";
+
+    // Set the current player to X
+    let current_player = player_X;
+
+    // Set Array to keep track of board
+    let board = Array(9).fill(null);
+
+
+    // Set functionality for squares on click
+    boardSquares.forEach((square, index) => {  // square: for each div in class square. i: the index number of the current square in the array
+        square.addEventListener(`click`, function(e){
+            let currentSquare = e.target // Saves the current square being manipulated in a variable
+
+            // Add X or O to a square
+            if(currentSquare.textContent != "X" && currentSquare.textContent != "O"){ 
+                currentSquare.textContent = current_player
+                currentSquare.classList.add(current_player) // Adds proper styling 
+                board[index] = current_player
+    
+                current_player =  current_player === player_X? player_O:player_X; // Alternates between X and O
+            }
+
+        });
+    }); 
+
+
 
 });
 
